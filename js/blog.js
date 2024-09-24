aed9f4a3b8ff633bed84628862bd7537c7290d3a
unknown <partha2165>
2024-09-24 16:34:04 +0600

    js Addedd

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,3 @@
 document.getElementById('btn-home').addEventListener('click', function(){
-    window.location.href = "../index.html"
+    window.location.href = "https://partha2165.github.io/B10-A5/";
 })
